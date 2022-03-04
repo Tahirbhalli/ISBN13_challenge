8450c26fef4860e84c071e71dd42c11b75027d4b
Tahir Ahmad <tahirbhalli16@gmail.com>
2022-03-04 19:06:16 +0500

    Update challenge.rb

--- challenge.rb
+++ challenge.rb
@@ -2,7 +2,7 @@ def calculate(num)
   num=num.to_s
   sum = 0
   num.length.times do |char|
-    sum += (char%2).zero? ? num[char].to_i * 1 : num[char].to_i * 3
+    sum += char.even? ? num[char].to_i * 1 : num[char].to_i * 3
   end
   num << (10 - (sum % 10)).to_s
 end
